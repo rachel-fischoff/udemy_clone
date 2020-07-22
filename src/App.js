59dfd275a32ddc32261092850321dcb92fde1b83
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-07-22 18:50:01 -0400

    with nav bar with login / sign up in main_nav_bar_home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import PrimarySearchAppBar from './components/main_nav_bar'
+import LoginSearchAppBar from './components/main_nav_bar_home'
 
 function App() {
   return (
     <div className="App">
-      <PrimarySearchAppBar/>
+      {/* <PrimarySearchAppBar/> */}
+      <LoginSearchAppBar/>
       
     </div>
   );
