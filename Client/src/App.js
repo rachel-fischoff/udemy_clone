c31cde9e4b1066ebfaac64f83ed364a7fec35cd0
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-07-25 13:30:00 -0400

    with client and server folders

---
+++ Client/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import PrimarySearchAppBar from "./components/main_nav_bar";
+import LoginSearchAppBar from "./components/main_nav_bar_home";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        {/* <ThemeProvider> */}
+          <CssBaseline />
+          {/* TODO: Have a nav bar that switches based on logged in or logged out? where do i put this component and how? */}
+          {/* <PrimarySearchAppBar /> */}
+          <LoginSearchAppBar />
+          {/* TODO:  create a <SecondaryNav /> */}
+
+          <Switch>
+            {/* <Route exact path="/"> TODO: include some redirects /home /courses */}
+            {/* <Home/> */}
+            {/* </Route> */}
+            {/* <Route exact path="/courses/:category> */}
+            {/* <CategoryPage/> */}
+            {/* </Route> */}
+            {/* <Route exact path="/courses/:category/:detail> */}
+            {/* <DetailPage/> */}
+            {/* </Route> */}
+          </Switch>
+        {/* </ThemeProvider> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Client/src/components/main_nav_bar.js
@@ -0,0 +1,289 @@
+import React, {useState} from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import AssignmentOutlinedIcon from "@material-ui/icons/AssignmentOutlined";
+import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import Avatar from "@material-ui/core/Avatar";
+
+const useStyles = makeStyles((theme) => ({
+
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  button: { marginLeft: theme.spacing(2) },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "160px",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "60ch",
+    },
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+  const [categoriesMoreAnchorEl, setCategoriesMoreAnchorEl] = useState(
+    null
+  );
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const isCategoriesMenuOpen = Boolean(categoriesMoreAnchorEl);
+
+  const handleClick = (event) => {
+    setCategoriesMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setCategoriesMoreAnchorEl(null);
+  };
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <AssignmentOutlinedIcon className={classes.menuButton} />
+
+          <Typography className={classes.title} variant="h6" noWrap>
+            eLearning Platform
+          </Typography>
+          {/* TODO: currently udemy has a drop down menu that drops on hover */}
+          <Button
+            className={classes.button}
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onMouseOver={handleClick}
+            // onClick={handleClick}
+          >
+            Categories
+          </Button>
+          <Menu
+            id="simple-menu"
+            variant="selectedMenu"
+            anchorEl={categoriesMoreAnchorEl}
+            anchorOrigin={{
+              vertical: "bottom", horizontal: "left"
+            }}
+            getContentAnchorEl={null}
+            keepMounted
+            open={Boolean(categoriesMoreAnchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Category1</MenuItem>
+            <MenuItem onClick={handleClose}>Category2</MenuItem>
+            <MenuItem onClick={handleClose}>Category3</MenuItem>
+          </Menu>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <Button
+            className={classes.button}
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onMouseOver={handleClick}
+          >
+            My Courses
+          </Button>
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <FavoriteBorderOutlinedIcon fontSize="large" />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon fontSize="large" />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCartOutlinedIcon fontSize="large" />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              {/* TODO: ADD a drawer from material UI and make it a button - 
+            should be a different component Persistent Drawer */}
+              <Avatar>RF</Avatar>
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
+}
