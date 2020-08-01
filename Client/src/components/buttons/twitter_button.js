37c02b1a70afe25e938ed42f9de214df9d6a12f7
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-07-31 20:05:03 -0400

    with bigger twitter button

--- Client/src/components/buttons/twitter_button.js
+++ Client/src/components/buttons/twitter_button.js
@@ -21,13 +21,6 @@ export default function TwitterButton() {
   return (
     <div style={{ marginTop: "80px" }} className={classes.paper}>
       <Card>
-        {/* <CardMedia
-          className={classes.image}
-          component="img"
-          height="30"
-          image={require("../../assets/sign-in-with-twitter-gray-1-png-img-fullhd-medium.png.img.fullhd.medium.png")}
-          //   onClick={handleTwitterSignIn}
-        /> */}
         <CardMedia
           className={classes.image}
           component="img"
