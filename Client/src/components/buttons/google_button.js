0ef3cd60b75265c26e1fa659294ef6202116abf6
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-07-31 20:33:42 -0400

    with google login button connecting to google

--- Client/src/components/buttons/google_button.js
+++ Client/src/components/buttons/google_button.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CardMedia from "@material-ui/core/CardMedia";
-import Card from "@material-ui/core/Card"
+import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -16,8 +16,8 @@ export default function GoogleButton() {
   const classes = useStyles();
 
   const handleGoogleSignIn = () => {
-    href  = "auth/google"
-  }
+    window.open("auth/google", "_blank");
+  };
 
   return (
     <div style={{ marginTop: "80px" }} className={classes.paper}>
@@ -26,7 +26,7 @@ export default function GoogleButton() {
           component="img"
           height="90"
           image={require("../../assets/btn_google_signin_dark_normal_web@2x.png")}
-          //   onClick={handleGoogleSignIn}
+          onClick={handleGoogleSignIn}
         />
       </Card>
     </div>
